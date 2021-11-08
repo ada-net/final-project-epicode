ca8555e5d1720e1fc04c11ee4eb1c9f842c43a9f
sabinefischer <sabine.pescatrice@gmail.com>
2021-11-08 22:45:10 +0100

    minor changes

--- accountingapp/src/app/login/login.component.ts
+++ accountingapp/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginData = {};
+  constructor(private authentication: LoginService) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    this.authentication.loginUser(this.loginData)
+    .subscribe(
+      res => console.log(res),
+      err => console.log(err)
+    )
+   
+  
+  
   }
 
 }
--- accountingapp/src/app/services/login.service.ts
+++ accountingapp/src/app/services/login.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class LoginService {
 
-  constructor() { }
+  private loginUrl = "http://localhost:4200/login";
+
+  constructor(private http: HttpClient) { }
+
+  loginUser(user: any){
+    return this.http.post<any>(this.loginUrl, user)
+  }
 }
